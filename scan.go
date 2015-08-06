b442aa38d73499573bdd48ca5e81b1a6c4f59ace
Richard Musiol <mail@richard-musiol.de>
2015-08-06 21:52:59 +0200

    fix path check for Godeps/_workspace

--- scan.go
+++ scan.go
@@ -287,7 +287,7 @@ func scanForPackages(dir string) ([]*build.Package, error) {
 	for _, info := range infos {
 		name := info.Name()
 		fullPath := filepath.Join(dir, name)
-		if info.IsDir() && ((name[0] != '.' && name[0] != '_' && name != "testdata") || (strings.HasSuffix(fullPath, "Godeps/_workspace") && !config.SkipGodeps)) {
+		if info.IsDir() && ((name[0] != '.' && name[0] != '_' && name != "testdata") || (strings.HasSuffix(filepath.ToSlash(fullPath), "/Godeps/_workspace") && !config.SkipGodeps)) {
 			subPkgs, err := scanForPackages(fullPath)
 			if err != nil {
 				return nil, err
