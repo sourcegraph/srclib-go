e17bc5f40fab1a7d14b822f14654b7a2b9322862
Beyang Liu <beyang.liu@gmail.com>
2016-01-02 19:31:05 -0800

    refactor

--- convert.go
+++ convert.go
@@ -12,42 +12,49 @@ import (
 func convertUnits(u0 []*unit.SourceUnit) ([]*graph2.Unit, error) {
 	u1 := make([]*graph2.Unit, len(u0))
 	for i, u := range u0 {
-		dataBytes, err := json.Marshal(u.Data)
+		var err error
+		u1[i], err = convertUnit(u)
 		if err != nil {
 			return nil, err
 		}
-
-		var key graph2.UnitKey
-		key.Genus = "git"
-		key.URI = u.Repo
-		key.Version = u.CommitID
-		key.UnitName = u.Name
-		key.UnitType = u.Type
-
-		var info *graph2.UnitInfo
-		if u.Info != nil {
-			info = &graph2.UnitInfo{
-				NameInRepository: u.Info.NameInRepository,
-				GlobalName:       u.Info.GlobalName,
-				Description:      u.Info.Description,
-				TypeName:         u.Info.TypeName,
-			}
-		}
-
-		newU := &graph2.Unit{
-			UnitKey: key,
-			Globs:   u.Globs,
-			Files:   u.Files,
-			Dir:     u.Dir,
-			// RawDeps: TODO(beyang),
-			// Deps: TODO(beyang),
-			DerivedFrom: nil,
-			Info:        info,
-			Data:        dataBytes,
-		}
-		u1[i] = newU
 	}
 	return u1, nil
+}
+
+func convertUnit(u *unit.SourceUnit) (*graph2.Unit, error) {
+	dataBytes, err := json.Marshal(u.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	var key graph2.UnitKey
+	key.Genus = "git"
+	key.URI = u.Repo
+	key.Version = u.CommitID
+	key.UnitName = u.Name
+	key.UnitType = u.Type
+
+	var info *graph2.UnitInfo
+	if u.Info != nil {
+		info = &graph2.UnitInfo{
+			NameInRepository: u.Info.NameInRepository,
+			GlobalName:       u.Info.GlobalName,
+			Description:      u.Info.Description,
+			TypeName:         u.Info.TypeName,
+		}
+	}
+
+	return &graph2.Unit{
+		UnitKey: key,
+		Globs:   u.Globs,
+		Files:   u.Files,
+		Dir:     u.Dir,
+		// RawDeps: TODO(beyang),
+		// Deps: TODO(beyang),
+		DerivedFrom: nil,
+		Info:        info,
+		Data:        dataBytes,
+	}, nil
 }
 
 func convertDefKey(d graph.DefKey) graph2.NodeKey {
